c7b55b47a7304fdd579918251c0f2911bd84f60d
gurrl9823 <gurrl9823@gmail.com>
2022-04-12 20:54:01 +0900

    수정

--- app.py
+++ app.py
@@ -8,26 +8,12 @@ app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-def order(symbol, positionSide, side, timestamp, order_type=ORDER_TYPE_MARKET):
-    try:
-        print(f"sending order {order_type} - {symbol} {positionSide} {side} ")
-        order = client.futures_create_order(
-            symbol=symbol,              # BTCUSDT
-            positionSide=positionSide,  # LONG, SHORT
-            side=side,                  # BUY, SELL
-            type=order_type,            # MARKET
-            timestamp=timestamp)        # time
-    except Exception as e:
-        print("an exception occured - {}".format(e))
-        return False
-
-    return order
-
-# def order(symbol, side, timestamp, order_type=ORDER_TYPE_MARKET):
+# def order(symbol, positionSide, side, timestamp, order_type=ORDER_TYPE_MARKET):
 #     try:
-#         print(f"sending order {order_type} - {symbol} {side} ")
+#         print(f"sending order {order_type} - {symbol} {positionSide} {side} ")
 #         order = client.futures_create_order(
 #             symbol=symbol,              # BTCUSDT
+#             positionSide=positionSide,  # LONG, SHORT
 #             side=side,                  # BUY, SELL
 #             type=order_type,            # MARKET
 #             timestamp=timestamp)        # time
@@ -36,6 +22,20 @@ def order(symbol, positionSide, side, timestamp, order_type=ORDER_TYPE_MARKET):
 #         return False
 #
 #     return order
+
+def order(symbol, side, timestamp, order_type=ORDER_TYPE_MARKET):
+    try:
+        print(f"sending order {order_type} - {symbol} {side} ")
+        order = client.futures_create_order(
+            symbol=symbol,              # BTCUSDT
+            side=side,                  # BUY, SELL
+            type=order_type,            # MARKET
+            timestamp=timestamp)        # time
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return False
+
+    return order
 
 
 @app.route('/')
@@ -58,8 +58,8 @@ def webhook():
     #timestamp =  data['bar']['time']
     timestamp = datetime.datetime.now().timestamp()
     print(timestamp)
-    order_response = order("BTCUSDT", positionSide, side, timestamp)
-    #order_response = order("BTCUSDT", side, timestamp)
+    # order_response = order("BTCUSDT", positionSide, side, timestamp)
+    order_response = order("BTCUSDT", side, timestamp)
 
     if order_response:
         return {
