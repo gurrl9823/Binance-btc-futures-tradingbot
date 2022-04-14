0eae52574d3c166eccdaa459298ae9f7d1337c1c
gurrl9823 <gurrl9823@gmail.com>
2022-04-14 15:21:56 +0900

    수정

--- app.py
+++ app.py
@@ -84,10 +84,10 @@ app = Flask(__name__)
 client = Client(config.API_KEY, config.API_SECRET)
 
 
-def order(side, quoteOrderQty, symbol, order_type=ORDER_TYPE_MARKET):
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
-        print(f"sending order {order_type} - {side} {quoteOrderQty} {symbol}")
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quoteOrderQty=quoteOrderQty)
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
@@ -112,9 +112,8 @@ def webhook():
         }
 
     side = data['strategy']['order_action'].upper()
-    # quantity = data['strategy']['order_contracts']
-    quoteOrderQty = 99
-    order_response = order(side, quoteOrderQty, "DOGEUSDT")
+    quantity = data['strategy']['order_contracts']
+    order_response = order(side, quantity, "DOGEUSDT")
 
     if order_response:
         return {
