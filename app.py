3b253a94057cee52c10d183eef47c4b6c60411a5
gurrl9823 <gurrl9823@gmail.com>
2022-04-14 09:31:54 +0900

    수정

--- app.py
+++ app.py
@@ -1,5 +1,79 @@
+# import json, config
+# import datetime
+# from flask import Flask, request, jsonify, render_template
+# from binance.client import Client
+# from binance.enums import *
+#
+# app = Flask(__name__)
+#
+# client = Client(config.API_KEY, config.API_SECRET, tld='us')
+#
+# # def order(symbol, positionSide, side, timestamp, order_type=ORDER_TYPE_MARKET):
+# #     try:
+# #         print(f"sending order {order_type} - {symbol} {positionSide} {side} ")
+# #         order = client.futures_create_order(
+# #             symbol=symbol,              # BTCUSDT
+# #             positionSide=positionSide,  # LONG, SHORT
+# #             side=side,                  # BUY, SELL
+# #             type=order_type,            # MARKET
+# #             timestamp=timestamp)        # time
+# #     except Exception as e:
+# #         print("an exception occured - {}".format(e))
+# #         return False
+# #
+# #     return order
+#
+# def order(symbol, side, positionSide, type):
+#     try:
+#         print(f"sending order {type} - {symbol} {side} ")
+#         order = client.futures_create_order(
+#             symbol=symbol,              # BTCUSDT
+#             side=side,                  # BUY, SELL
+#             positionSide=positionSide,  # LONG, SHORT
+#             type=type)                  # MARKET
+#     except Exception as e:
+#         print("an exception occured - {}".format(e))
+#         return False
+#
+#     return order
+#
+#
+# @app.route('/')
+# def welcome():
+#     return render_template('index.html')
+#
+# @app.route('/webhook', methods=['POST'])
+# def webhook():
+#     #print(request.data)
+#     data = json.loads(request.data)
+#
+#     if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
+#         return {
+#             "code": "error",
+#             "message": "Nice try, invalid passphrase"
+#         }
+#
+#     side = data['strategy']['order_action'].upper() #BUY, SELL
+#     positionSide = data['strategy']['market_position'].upper() #LONG, SHORT
+#     type = 'MARKET'
+#     quantity = 0.04
+#     order_response = order("BTCUSDT", side, positionSide, type)
+#
+#     if order_response:
+#         return {
+#             "code": "success",
+#             "message": "order executed"
+#         }
+#     else:
+#         print("order failed")
+#
+#         return {
+#             "code": "error",
+#             "message": "order failed"
+#         }
+
+
 import json, config
-import datetime
 from flask import Flask, request, jsonify, render_template
 from binance.client import Client
 from binance.enums import *
@@ -8,29 +82,11 @@ app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
-# def order(symbol, positionSide, side, timestamp, order_type=ORDER_TYPE_MARKET):
-#     try:
-#         print(f"sending order {order_type} - {symbol} {positionSide} {side} ")
-#         order = client.futures_create_order(
-#             symbol=symbol,              # BTCUSDT
-#             positionSide=positionSide,  # LONG, SHORT
-#             side=side,                  # BUY, SELL
-#             type=order_type,            # MARKET
-#             timestamp=timestamp)        # time
-#     except Exception as e:
-#         print("an exception occured - {}".format(e))
-#         return False
-#
-#     return order
 
-def order(symbol, side, positionSide, type):
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
-        print(f"sending order {type} - {symbol} {side} ")
-        order = client.futures_create_order(
-            symbol=symbol,              # BTCUSDT
-            side=side,                  # BUY, SELL
-            positionSide=positionSide,  # LONG, SHORT
-            type=type)                  # MARKET
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
@@ -42,9 +98,10 @@ def order(symbol, side, positionSide, type):
 def welcome():
     return render_template('index.html')
 
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    #print(request.data)
+    # print(request.data)
     data = json.loads(request.data)
 
     if data['passphrase'] != config.WEBHOOK_PASSPHRASE:
@@ -53,11 +110,9 @@ def webhook():
             "message": "Nice try, invalid passphrase"
         }
 
-    side = data['strategy']['order_action'].upper() #BUY, SELL
-    positionSide = data['strategy']['market_position'].upper() #LONG, SHORT
-    type = 'MARKET'
-    quantity = 0.04
-    order_response = order("BTCUSDT", side, positionSide, type)
+    side = data['strategy']['order_action'].upper()
+    quantity = data['strategy']['order_contracts']
+    order_response = order(side, quantity, "BTCUSDT")
 
     if order_response:
         return {
