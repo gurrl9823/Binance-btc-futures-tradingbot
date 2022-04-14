73fcbb58e272297c2c73a98bfedd00436efb7559
gurrl9823 <gurrl9823@gmail.com>
2022-04-14 16:29:13 +0900

    수정

--- app.py
+++ app.py
@@ -85,10 +85,10 @@ client = Client(config.API_KEY, config.API_SECRET)
 # request_client = RequestClient(api_key = config.API_KEY, secret_key = config.API_SECRET)
 
 
-def order(side, quoteOrderQty, symbol, order_type=ORDER_TYPE_MARKET):
+def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
     try:
-        print(f"sending order {order_type} - {side} {quoteOrderQty} {symbol}")
-        order = client.create_order(symbol=symbol, side=side, type=order_type, quoteOrderQty=quoteOrderQty)
+        print(f"sending order {order_type} - {side} {quantity} {symbol}")
+        order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
@@ -115,8 +115,8 @@ def webhook():
     side = data['strategy']['order_action'].upper()
     # account = request_client.get_account_information()
     # quantity = 99.9 # 22/data['strategy']['order_price'] #data['strategy']['order_contracts']
-    quoteOrderQty = 100
-    order_response = order(side, quoteOrderQty, "DOGEUSDT")
+    quantity = 100
+    order_response = order(side, quantity, "DOGEUSDT")
 
     if order_response:
         return {
