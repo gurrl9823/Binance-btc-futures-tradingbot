15f52c5859fc24c013994a6ba1f5ad43d5f07948
gurrl9823 <gurrl9823@gmail.com>
2022-04-24 21:25:09 +0900

    수정

--- app.py
+++ app.py
@@ -38,24 +38,25 @@ def webhook():
 
 
 
-    try:
-        # 포지션 정리
-        client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=executedQty)
-
-        # 최대 구매 가능 코인 계산
-        maxWithdrawAmount = float(client.futures_account()['maxWithdrawAmount'])
-        leverage = 5
-        print(maxWithdrawAmount)
-        quantity = math.floor(maxWithdrawAmount * leverage / data['strategy']['order_price'] * 1000) / 1000
-        print(quantity)
-
-        # 포지션 진입
-        order_response = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-        print(f"sending order {side} {symbol} {order_type} {quantity} ")
-
-    except Exception as e:
-        print("an exception occured - {}".format(e))
-        return False
+    # try:
+    #     # 포지션 정리
+    #     if x == long
+    #         client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=executedQty)
+    #
+    #     # 최대 구매 가능 코인 계산
+    #     maxWithdrawAmount = float(client.futures_account()['maxWithdrawAmount'])
+    #     leverage = 15
+    #     print(maxWithdrawAmount)
+    #     quantity = math.floor(maxWithdrawAmount * leverage / data['strategy']['order_price'] * 1000) / 1000
+    #     print(quantity)
+    #
+    #     # 포지션 진입
+    #     order_response = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+    #     print(f"sending order {side} {symbol} {order_type} {quantity} ")
+    #
+    # except Exception as e:
+    #     print("an exception occured - {}".format(e))
+    #     return False
 
 
     if order_response:
