f9cc332da59e034333db65ad8e851128a7bb800b
gurrl9823 <gurrl9823@gmail.com>
2022-04-26 10:44:37 +0900

    수정

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET)
 
+
 @app.route('/')
 def welcome():
     return render_template('index.html')
@@ -24,7 +25,6 @@ def webhook():
             "message": "Nice try, invalid passphrase"
         }
 
-
     symbol = "BTCUSDT"
 
     # 현재 포지션의 코인 갯수
@@ -32,19 +32,22 @@ def webhook():
     executedQty = a[-1]['executedQty']
     print('현재 포지션의 코인 개수 : ', executedQty)
 
-    side = data['strategy']['order_action'].upper() # buy, sell
+    side = data['strategy']['order_action'].upper()  # buy, sell
 
     order_type = "MARKET"
-
 
     try:
         # 롱 포지션 정리
         if (data['strategy']['order_id'] == 'exit') and (data['strategy']['prev_market_position'] == 'long'):
-            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=data['strategy']['order_price'], closePosition='true')
+            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET',
+                                                         stopPrice=data['strategy']['order_price'],
+                                                         closePosition='true')
             print(f"sending order {side} {symbol} STOP_MARKET")
         # 숏 포지션 정리
         elif (data['strategy']['order_id'] == 'exit') and (data['strategy']['prev_market_position'] == 'short'):
-            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=data['strategy']['order_price']-100, closePosition='true')
+            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET',
+                                                         stopPrice=data['strategy']['order_price'] - 100,
+                                                         closePosition='true')
             print(f"sending order {side} {symbol} STOP_MARKET")
         # 포지션 진입
         else:
@@ -56,12 +59,11 @@ def webhook():
             print("구매 가능한 코인 개수 : ", quantity)
 
             order_response = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
-            print(f"sending order {side} {symbol} {order_type} {maxWithdrawAmount*15}$ {quantity} ")
+            print(f"sending order {side} {symbol} {order_type} {maxWithdrawAmount * 15}$ {quantity} ")
 
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
-
 
     if order_response:
         print("order executed")
