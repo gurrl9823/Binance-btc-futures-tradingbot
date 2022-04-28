b88bffc71b8476d884dd471c74d9e58db2eb7abd
gurrl9823 <gurrl9823@gmail.com>
2022-04-28 09:35:31 +0900

    수정

--- app.py
+++ app.py
@@ -40,12 +40,12 @@ def webhook():
         # 롱 포지션 정리
         if (data['strategy']['order_id'] == 'exit') and (data['strategy']['prev_market_position'] == 'long'):
             # order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=data['strategy']['order_price'], closePosition='true')
-            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=0, closePosition='true')
+            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=100, closePosition='true')
             print(f"sending order {side} {symbol} STOP_MARKET")
         # 숏 포지션 정리
         elif (data['strategy']['order_id'] == 'exit') and (data['strategy']['prev_market_position'] == 'short'):
             # order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=data['strategy']['order_price'] - 100, closePosition='true')
-            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=0, closePosition='true')
+            order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET', stopPrice=100, closePosition='true')
             print(f"sending order {side} {symbol} STOP_MARKET")
         # 포지션 진입
         else:
