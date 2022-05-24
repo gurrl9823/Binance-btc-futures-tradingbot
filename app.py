9d10285407da01c97fe525d1ed24e3f685e4edbc
gurrl9823 <gurrl9823@gmail.com>
2022-05-24 15:05:07 +0900

    수정

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 client = Client(config.API_KEY, config.API_SECRET)
 request_client = RequestClient(api_key=config.API_KEY, secret_key=config.API_SECRET)
 
-# global present_order_id
-# present_order_id = ''
-
 @app.route('/')
 def welcome():
     return render_template('index.html')
@@ -20,7 +17,6 @@ def welcome():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    # global present_order_id
     # print(request.data)
     data = json.loads(request.data)
     print(data['passphrase'])
@@ -36,7 +32,6 @@ def webhook():
         }
 
 
-
     if data['passphrase'] == "don't sleep~" :
         return {
             "message": "don't sleep~"
@@ -44,28 +39,32 @@ def webhook():
 
 
 
+    symbol = "BTCUSDT"
+
+    side = data['strategy']['order_action'].upper()  # buy, sell
+
+    order_type = "MARKET"
+
+    stopPrice = int(float(data['strategy']['order_price']) * 0.98)
+
+    present_order_id = ''
+    if data['strategy']['prev_market_position_size'] != 0:
+        a = client.futures_get_all_orders(symbol=symbol)
+        present_order_id = a[-1]['clientOrderId']
+        print(present_order_id)
+
+        b = client.futures_recent_trades()
+        print(b)
+
+
+
 
 
     if data['passphrase'] == "4h 497d 846%" :
-
-        symbol = "BTCUSDT"
-
         # 현재 포지션의 코인 갯수
         # a = client.futures_get_all_orders(symbol=symbol)
         # origQty = a[-1]['origQty']
         # print('현재 포지션의 코인 개수 : ', origQty)
-        present_order_id = ''
-        if data['strategy']['prev_market_position_size'] != 0 :
-            a = client.futures_get_all_orders(symbol=symbol)
-            present_order_id = a[-1]['clientOrderId']
-            print(present_order_id)
-
-        side = data['strategy']['order_action'].upper()  # buy, sell
-
-        order_type = "MARKET"
-
-        stopPrice = int(float(data['strategy']['order_price'])*0.98)
-
         try:
             # 포지션 정리
             if ((data['strategy']['order_id'] == '1exit') or (data['strategy']['order_id'] == 'Close entry(s) order 1Long') or (data['strategy']['order_id'] == 'Close entry(s) order 1Short')) and (present_order_id == '4h_497d_846p'):
@@ -88,7 +87,6 @@ def webhook():
                     client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=origQty)
 
                 # 최대 구매 가능 코인 계산
-                # maxWithdrawAmount = math.floor(float(client.futures_account()['maxWithdrawAmount']) / 100) * 100
                 maxWithdrawAmount = float(client.futures_account()['maxWithdrawAmount']) * 0.99
                 leverage = 3
                 print("현재 구매 가능한 달러 : ", maxWithdrawAmount)
@@ -141,16 +139,9 @@ def webhook():
 
     if data['passphrase'] == "30m 871d 40%" :
 
-        symbol = "BTCUSDT"
-
-        side = data['strategy']['order_action'].upper()  # buy, sell
-
-        order_type = "MARKET"
-
         try:
             # 포지션 정리
-            if (data['strategy']['order_id'] == '2exit') and ((present_order_id == '2Long') or (present_order_id == '2Short')):
-                present_order_id = ''
+            if (data['strategy']['order_id'] == '2exit') and (present_order_id == '30m_871d_40p'):
                 order_response = client.futures_create_order(symbol=symbol, side=side, type='STOP_MARKET',
                                                              stopPrice=1000, closePosition='true')
                 print(f"Close position : {data['strategy']['order_id']} {side} {symbol} STOP_MARKET")
@@ -158,14 +149,13 @@ def webhook():
             elif data['strategy']['prev_market_position_size'] == 0:
                 present_order_id = data['strategy']['order_id']
                 # 최대 구매 가능 코인 계산
-                # maxWithdrawAmount = math.floor(float(client.futures_account()['maxWithdrawAmount']) / 100) * 100
                 maxWithdrawAmount = float(client.futures_account()['maxWithdrawAmount'])
                 leverage = 3
                 print("현재 구매 가능한 달러 : ", maxWithdrawAmount)
                 quantity = math.floor(((maxWithdrawAmount * leverage) / data['strategy']['order_price']) * 1000) / 1000
                 print("구매 가능한 코인 개수 : ", quantity)
 
-                order_response = client.futures_create_order(symbol=symbol, side=side, type=order_type,
+                order_response = client.futures_create_order(newClientOrderId='30m_871d_40p', symbol=symbol, side=side, type=order_type,
                                                              quantity=quantity)
                 print(f"entry position : {data['strategy']['order_id']} {side} {symbol} {order_type} {maxWithdrawAmount * leverage}$ {quantity} ")
 
@@ -185,6 +175,3 @@ def webhook():
                 "code": "error",
                 "message": "order failed"
             }
-
-
-
